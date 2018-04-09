a5a38969c017511ac098acf5233a0c9fb1ec376d
caoxingyu <caoxingyu2016@qq.com>
2018-04-09 16:58:08 +0800

    下单

--- src/main/java/com/jielong/core/service/impl/OrderServiceImpl.java
+++ src/main/java/com/jielong/core/service/impl/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package com.jielong.core.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +11,8 @@ import com.jielong.core.beans.ResponseBean;
 import com.jielong.core.dao.CommonDao;
 import com.jielong.core.dao.OrderMapper;
 import com.jielong.core.domain.Order;
+import com.jielong.core.domain.OrderGoods;
+import com.jielong.core.service.OrderGoodsService;
 import com.jielong.core.service.OrderService;
 
 @Service
@@ -17,7 +21,9 @@ public class OrderServiceImpl implements OrderService{
 	@Autowired
 	CommonDao commonDao;
 	@Autowired
-	OrderMapper orderMapper;
+	OrderMapper orderMapper;	
+	@Autowired
+	OrderGoodsService orderGoodsService;
 	
 	@Transactional
 	@Override
@@ -27,7 +33,19 @@ public class OrderServiceImpl implements OrderService{
         order.setOrderNum(orderNum);
         //订单状态
         order.setState(1);
-		orderMapper.insertSelective(order);
+		orderMapper.insertSelective(order);		
+		Integer orderId=commonDao.getLastId();
+		List<OrderGoods> orderGoodsList=order.getOrderGoods();
+		if (orderGoodsList!=null && orderGoodsList.size()>0) {
+			for(int i=0;i<orderGoodsList.size();i++) {
+			   OrderGoods orderGoods=orderGoodsList.get(i);
+			   orderGoods.setOrderId(orderId);
+			   orderGoodsService.insert(orderGoods);
+			}
+		}
+		
+		
+		
 		return null;
 	}
 }
